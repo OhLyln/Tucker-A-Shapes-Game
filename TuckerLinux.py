75c270f603c055e82d018095f7987ad10c18ce7d
Luke Tarr <ohlyln@gmail.com>
2017-01-14 22:39:26 -0500

    Update TuckerLinux.py

--- TuckerLinux.py
+++ TuckerLinux.py
@@ -4,7 +4,7 @@ import os
 
 
 # Welcomes the user to the program
-print("Welcome to Luke's Shape Test! \n")
+print("Welcome to Tucker! \n")
 
 # Needed for my infinite, but breakable, while-loop, and to create the shape
 sides = 0
